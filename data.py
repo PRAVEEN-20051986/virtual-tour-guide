f017df6ced156990a4a1ca1e9b4ab27b882c2cb6
Praveen-20051986 <mpm52338@gmail.com>
2026-01-09 10:48:59 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,16 @@
+tour_data = {
+    "ooty": {
+        "stays": [
+            {"name": "Green Hills Resort", "price": "₹2500/night"},
+            {"name": "Lake View Lodge", "price": "₹1800/night"}
+        ],
+        "cars": [
+            {"name": "Innova", "price": "₹3000/day"},
+            {"name": "Swift", "price": "₹2200/day"}
+        ],
+        "bikes": [
+            {"name": "Royal Enfield", "price": "₹1200/day"},
+            {"name": "Activa", "price": "₹600/day"}
+        ]
+    }
+}
